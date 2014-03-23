273fb26959b06e04bbcf06dab8051b76e2676ad9
Joseph George Variam <joseph@variam.com>
2014-03-23 15:59:41 +0530

    New services added

---
+++ src/com/joppu/webcam/ImageProcessingService.java
@@ -0,0 +1,129 @@
+package com.joppu.webcam;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+
+import java.io.File;
+
+public class ImageProcessingService {
+
+    private static final ImageProcessingService imageProcessingService = new ImageProcessingService();
+
+    private ImageProcessingService(){
+
+    }
+
+    public static ImageProcessingService getInstance(){
+        return imageProcessingService;
+    }
+
+    public static void main(String args[]) throws Exception{
+        ImageProcessingService.getInstance().start();
+    }
+
+    private void start() throws Exception{
+        Runnable monitorFolder0 = new MonitorFolderRunnable("img/camera0");
+        Runnable monitorFolder1 = new MonitorFolderRunnable("img/camera1");
+
+        Thread worker0 = new Thread(monitorFolder0);
+        Thread worker1 = new Thread(monitorFolder1);
+
+        worker0.start();
+        worker1.start();
+    }
+
+    private void uploadImage(File file) throws Exception{
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpPost httppost = new HttpPost("http://localhost:8080" +"/servlets-examples/servlet/RequestInfoExample");
+
+            FileBody bin = new FileBody(file);
+
+            HttpEntity reqEntity = MultipartEntityBuilder.create()
+                    .addPart("bin", bin)
+                    .build();
+
+            httppost.setEntity(reqEntity);
+
+            System.out.println("executing request " + httppost.getRequestLine());
+            CloseableHttpResponse response = httpclient.execute(httppost);
+            try {
+                System.out.println("----------------------------------------");
+                System.out.println(response.getStatusLine());
+                HttpEntity resEntity = response.getEntity();
+                if (resEntity != null) {
+                    System.out.println("Response content length: " + resEntity.getContentLength());
+                }
+                EntityUtils.consume(resEntity);
+            } finally {
+                response.close();
+            }
+        } finally {
+            httpclient.close();
+        }
+    }
+
+
+    private void moitorFolder(String path) throws Exception{
+        Path folder = Paths.get(path);
+        WatchService watchService = FileSystems.getDefault().newWatchService();
+        folder.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
+
+        boolean valid = true;
+        do {
+            WatchKey watchKey = watchService.take();
+
+            for (WatchEvent<?> event : watchKey.pollEvents()) {
+                WatchEvent.Kind kind = event.kind();
+                if (StandardWatchEventKinds.ENTRY_CREATE.equals(event.kind())) {
+                    String fileName = event.context().toString();
+                    System.out.println("File Created:" + fileName);
+                    processFile(fileName);
+                }
+            }
+            valid = watchKey.reset();
+
+        } while (valid);
+    }
+
+    private synchronized void processFile(String fileName) {
+        try {
+            uploadImage(new File(fileName));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    class MonitorFolderRunnable implements Runnable{
+
+        private final String path;
+
+        public MonitorFolderRunnable(String path){
+            this.path = path;
+        }
+
+        @Override
+        public void run() {
+            try {
+                moitorFolder(path);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ src/com/joppu/webcam/StreamingServer.java
@@ -0,0 +1,67 @@
+package com.joppu.webcam;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
+@Path("/fileupload")
+public class StreamingServer {
+    private static StreamingServer ourInstance = new StreamingServer();
+
+    public static StreamingServer getInstance() {
+        return ourInstance;
+    }
+
+    private StreamingServer() {
+    }
+
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadFile(
+            @FormDataParam("file") InputStream uploadedInputStream,
+            @FormDataParam("file") FormDataContentDisposition fileDetail) {
+
+        String uploadedFileLocation = "c://uploadedFiles/" + fileDetail.getFileName();
+
+        // save it
+        saveToFile(uploadedInputStream, uploadedFileLocation);
+
+        String output = "File uploaded via Jersey based RESTFul Webservice to: " + uploadedFileLocation;
+
+        return Response.status(200).entity(output).build();
+
+    }
+
+    // save uploaded file to new location
+    private void saveToFile(InputStream uploadedInputStream,
+                            String uploadedFileLocation) {
+
+        try {
+            OutputStream out = null;
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            out = new FileOutputStream(new File(uploadedFileLocation));
+            while ((read = uploadedInputStream.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+    }
+
+}
